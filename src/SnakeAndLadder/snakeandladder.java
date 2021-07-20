e83476b8645f9ab6e6d3e86a8f03b3b1393e0600
Rohit Patro <rohitpatro6@gmail.com>
2021-07-21 00:08:52 +0530

    Added:Die Rolling Added

--- src/SnakeAndLadder/snakeandladder.java
+++ src/SnakeAndLadder/snakeandladder.java
@@ -6,6 +6,8 @@ public class snakeandladder {
 		System.out.println("Welcome To Snake And Ladder");
 
          int startPosition=0;
+         int DieRolled = (int) (Math.random()*6)+1;
+         System.out.println(DieRolled);
 		}
 			
 	}
